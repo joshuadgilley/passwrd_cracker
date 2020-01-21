428d8c9644c48fb99840ee0ea1df54980a39d259
joshuadgilley <joshdgilley@gmail.com>
2020-01-20 21:59:43 -0500

    updated using great salting method

--- Gilley_Taylor_HW1_passwd_cracking.py
+++ Gilley_Taylor_HW1_passwd_cracking.py
@@ -2,10 +2,14 @@
 # encryption: transfering file from one computer to another
 # hashing: better for hashing (add salt, etc.)
 
-import hashlib
+import hashlib, binascii
+# 100000 iteration sha256 hashing of that hash of that hash and so on...
+# great hashing with salt method
+dk = hashlib.pbkdf2_hmac('sha256', b'hello', b's', 100000)
+print(binascii.hexlify(dk));
 
-crypt = hashlib.sha256()
-crypt.update(b"hello") # b converts it into binary encoding "unicode-object"
-print(crypt.hexdigest()) # converts string to sha256 then to hex form and prints out
-print(crypt.digest_size)
-
+# crypt = hashlib.sha256()
+# crypt.update(b"hello") # b converts it into binary encoding "unicode-object"
+# print(crypt.hexdigest()) # converts string to sha256 then to hex form and prints out
+# print(crypt.digest_size)
+# print(hashlib.algorithms_available)
