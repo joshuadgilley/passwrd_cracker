afcf857840b005d038ac98265f2c658ae0ed91e2
Chris Taylor <chrismtaylor1998@gmail.com>
2020-01-24 15:09:11 -0500

    init commit

--- Gilley_Taylor_HW1_passwd_cracking.py
+++ Gilley_Taylor_HW1_passwd_cracking.py
@@ -8,15 +8,19 @@ import hashlib, binascii
 # dk = hashlib.pbkdf2_hmac('sha256', b'hello', b's', 100000)
 # print(binascii.hexlify(dk))
 
-crypt = hashlib.sha256()
-crypt.update(b"programming") # b converts it into binary encoding "unicode-object"
-print(crypt.hexdigest()) # converts string to sha256 then to hex form and prints out
-print(crypt.digest_size)
-print(hashlib.algorithms_available)
+def main():
+
+    crypt = hashlib.sha256()
+    crypt.update(b"programming") # b converts it into binary encoding "unicode-object"
+    print(crypt.hexdigest()) # converts string to sha256 then to hex form and prints out
+    print(crypt.digest_size)
+    print(hashlib.algorithms_available)
 
 
-def length(string):
-    print(len(string))
+    infile = open("words.txt", 'r')
 
-length('ab2620f9b7154d9f9dc1b3c2d949d85d595fe77f45411b3dbe6e5b47da564177')
-length('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+    data = infile.read()
+
+    print(data)
+
+main()
