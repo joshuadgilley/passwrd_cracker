e298d9802eeabae7f12bb57b922979429d19b315
Chris Taylor <chrismtaylor1998@gmail.com>
2020-01-26 17:05:34 -0500

    five digit function

--- Gilley_Taylor_HW1_passwd_cracking.py
+++ Gilley_Taylor_HW1_passwd_cracking.py
@@ -3,10 +3,51 @@
 # hashing: better for hashing (add salt, etc.)
 
 import hashlib, binascii
+from itertools import *
 # 100000 iteration sha256 hashing of that hash of that hash and so on...
 # great hashing with salt method
 # dk = hashlib.pbkdf2_hmac('sha256', b'hello', b's', 100000)
 # print(binascii.hexlify(dk))
+
+## Create a function that returns a list of possible numbers to hash
+
+# returns a list of all of the cartesian product of !,#,*,~, 0...9
+# first digit must be a special character
+def five_digit_list():
+    # creating cartesian product
+    last_four = product('0123456789#!*~', repeat=4)
+
+    # creating cartesian and final list
+    cart = []
+    count = 0
+    final = []
+
+    # creating a list of the cartesian products and adding them to cartesian list
+    for it in last_four:
+        string = ""
+        for element in it:
+            string += str(element)
+
+        cart.append(string)
+
+    beg = ['!', '*', '#', '~']
+
+    for i in beg:
+        for j in cart:
+            final.append(i+j)
+            count += 1
+
+    return final
+
+
+
+
+
+
+
+
+
+
 
 def main():
 
@@ -22,5 +63,6 @@ def main():
     data = infile.read()
 
     print(data)
+    five_digit_list()
 
 main()
