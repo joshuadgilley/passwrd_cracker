53396338024fbb2c6a39ec6f0592b7fd47aafab7
Chris Taylor <chrismtaylor1998@gmail.com>
2021-04-22 21:13:18 -0400

    fixed upload

--- Gilley_Taylor_HW1_passwd_cracking.py
+++ Gilley_Taylor_HW1_passwd_cracking.py
@@ -1,56 +1,272 @@
-# cryptography: making sensitive information unreadable and thus secure
-# encryption: transfering file from one computer to another
-# hashing: better for hashing (add salt, etc.)
+#################################################
+# Josh Gilley and Chris Taylor                ###
+# College of Charleston CS / Network Security ###
+# January 30, 2020                            ###
+#################################################
+
+# import hashing and graphics library
+import hashlib
+from itertools import product
+import time
 
 
-# 100000 iteration sha256 hashing of that hash of that hash and so on...
-# great hashing with salt method
-# dk = hashlib.pbkdf2_hmac('sha256', b'hello', b's', 100000)
-# print(binascii.hexlify(dk))
+# create cartesian array only one time
+cartesian = None
 
-#crypt = hashlib.sha256()
-#crypt.update(b"programming") # b converts it into binary encoding "unicode-object"
-#print(crypt.hexdigest()) # converts string to sha256 then to hex form and prints out
-#print(crypt.digest_size)
-#print(hashlib.algorithms_available)
-
-
-#def length(string):
-    #print(len(string))
-
-# length('ab2620f9b7154d9f9dc1b3c2d949d85d595fe77f45411b3dbe6e5b47da564177')
-#
-#
-# length('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
-
-import hashlib, binascii
-
-hexCode = "Maggie:ab2620f9b7154d9f9dc1b3c2d949d85d595fe77f45411b3dbe6e5b47da564177:42:20:MaggieSimpson:/home/maggie:/bin/tcsh"
-
-list = hexCode.split(":")
-
-password = list[1]
-
-print(password)
-
-# fileIO
-
-infile = open("words.txt", "r")
-count = 0
-
-for line in infile:
-
-    line = line.strip()
-
+# crypt function
+def cryptic(word_or_num):
     crypt = hashlib.sha256()
-    crypt.update(line.encode('utf-8'))
-    newPass = crypt.hexdigest()
+    crypt.update(word_or_num.encode('utf-8'))
+    encrypted = crypt.hexdigest()
+    return encrypted
 
-    print(newPass)
+# VALIDATION METHODS
+def seven_letter_cap(word):
+    valid = False
 
-    if password == newPass:
-        print(line + " is the password...")
-        break
+    if len(word) == 7 and word.isalpha:
+        valid = True
+
+    return valid
+
+
+def five_digit_special_begin(num):
+    valid = False
+
+    if ('*' in num[:1] or '~' in num[:1] or '!' in num[:1] or '#' in num[:1]) and len(num) == 5:
+        valid = True
+
+    return valid
+
+
+def five_letter_a_l_switch(word):
+    valid = False
+
+    if len(word) == 5 and word.isalpha() and ((word.find('a') != -1 or word.find('l') != -1)):
+        valid = True
+
+    return valid
+
+
+def up_to_seven_digits(num):
+    valid = False
+
+    if len(num) <= 7 and num.isnumeric():
+        valid = True
+
+    return valid
+
+
+def  single_words_no_spaces(word):
+    valid = False
+
+    if word.isalpha() and word.find(" ") == -1:
+        valid = True
+
+    return valid
+
+
+# seven_letter_cap_func that returns the list
+def seven_letter_cap_func(word):
+    word_list = []
+    i = 0
+    while i < 10:
+        word_list.append(word.capitalize() + str(i))
+        i += 1
+    return word_list
+
+
+# MAIN RULES METHOD (USING VALIDATION METHODS)
+def rules(word):
+    # A five char word with the letter 'a' in it which gets replaced with the special
+    # character @ and the character ‘l’ is substituted by the number ‘1’.
+    if five_letter_a_l_switch(word):
+
+        changed_word = ""
+
+        for i in range(len(word)):
+            if word[i] == 'a':
+                changed_word += "@"
+
+            elif word[i] == 'l':
+                changed_word += "1"
+
+            else:
+                changed_word += word[i]
+
+        return changed_word
+
+    # Any number of chars single word from /usr/share/dict/words (Linux or Mac)
+    elif single_words_no_spaces(word):
+        return str(word)
+
+    # If dictionary word doesn't match rules, return empty string
+    else:
+        return ""
+
+
+# Rule #4 any word that is made with digits up to 7 digits length.
+def up_to_seven_digits():
+
+    global cartesian
+
+    if (cartesian == None):
+        count = 0
+        i = 7
+        cart = []
+
+        while(i > 0):
+            prod = product('0123456789', repeat=i)
+
+            for it in prod:
+                string = ""
+                count += 1
+                for element in it:
+                    string += str(element)
+                cart.append(string)
+            i -= 1
+        cartesian = cart
+        return cart
+
+    else:
+        return cartesian
+
+def cart_helper(cart, password):
+
+    for it in cart:
+        if password == cryptic(it):
+            return it
+
+
+def five_digit_list():
+    # creating cartesian product
+    last_four = product('0123456789#!*~', repeat=4)
+    # creating cartesian and final list
+    cart = []
+    count = 0
+    final = []
+    # creating a list of the cartesian products and adding them to cartesian list
+    for it in last_four:
+        string = ""
+        for element in it:
+            string += str(element)
+        cart.append(string)
+    beg = ['!', '*', '#', '~']
+    for i in beg:
+        for j in cart:
+            final.append(i+j)
+            count += 1
+    return final
+
+
+
+def r_w_passwords(userIn, userOut):
+
+    total_time = 0.0
+    pass_count = 0
+
+    if userIn == "no_args" and userOut == "no_args":
+        userfile = input("Please enter the .txt file with 'username:password:other' on each line: ")
+        user_out = input("Please enter the .txt file you would like to write to:")
+
+    else:
+        userfile = userIn
+        user_out = userOut
+
+    print()
+    infile = open(userfile, "r")
+    outfile = open(user_out, "w")
+
+    for line in infile:
+        pass_count += 1
+        start = time.time()
+        username = line.split(":")[0]
+        encrypted_password = line.split(":")[1]
+        print("Trying to crack " + username + "'s password... ")
+        cracked = main(line)
+        if cracked is not None:
+
+            print("Success!")
+            print("Encrypted:password is: " + encrypted_password + ":" + cracked)
+            print(encrypted_password + ":" + cracked, file=outfile)
+
+
+
+
+        else:
+            print("Unsuccessful :^(")
+            print(username + "'s password couldn't be found...", file=outfile)
+
+        endtime = time.time() - start
+        total_time += endtime
+        print("It took {0:0.2f} seconds to execute".format(endtime))
+        print()
+        print("-------------------------------------")
+        print()
+
+    print("Wrote passwords to " + user_out)
+    print("Total time to crack " + str(pass_count) + " passwords in " + userfile + ": {0:0.2f} seconds".format(total_time))
+
+
+def main(file_line):
+
+    # open .txt file
+    infile = open("words.txt", "r")
+
+    password = file_line.split(":")[1].strip()
+
+    # cycle through each word in file
+    for line in infile:
+
+        # clean word of any added additional (/n /t etc..)
+        line = line.strip()
+
+        # seven char word which gets the first letter capitalized and a 1-digit number appended.
+        # if word qualifies, this creates a list of words with the ^ specifications
+        if seven_letter_cap(line):
+            fin = seven_letter_cap_func(line)
+
+        # if word is not seven_letter_cap list this fin = tuple, word, int, etc.
+        else:
+            fin = rules(line)
+
+        # if fin is a List, create hashes for every element and compare
+        if isinstance(fin, list):
+
+            for it in fin:
+                new_pass = cryptic(it)
+                if password == new_pass:
+                    # post cracked password
+                    return it
+
+        # if word doesn't fit rules, skip it
+        elif fin == "":
+            continue
+
+        # if fin is a tuple/string the hash it and compare!
+        elif not(isinstance(fin, list)):
+            new_pass = cryptic(fin)
+            if password == new_pass:
+                return fin
+
+
+    # Here we have broken out of the dictionary meaning that we need to check rule #2 and #4
+    five_digit_product = five_digit_list()
+    for it in five_digit_product:
+        if password == cryptic(it):
+            return it
+
+    cartesian_product = up_to_seven_digits()
+    for it in cartesian_product:
+        if password == cryptic(it):
+            return it
+
+    return None
+
+
+if __name__ == '__main__':
+    r_w_passwords("no_args", "no_args")
+
 
 
 
